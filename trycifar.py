1f5531eac26ca90b986774dbd99a234b19da7404
Deric Pang <dericp@cs.washington.edu>
2018-11-13 15:45:46 -0800

    Adding in answers from last time.

--- trycifar.py
+++ trycifar.py
@@ -1,6 +1,17 @@
 from uwnet import *
 
+def softmax_model():
+    l = [make_connected_layer(3072, 10, SOFTMAX)]
+    return make_net(l)
+
+def neural_net():
+    l = [   make_connected_layer(3072, 32, LRELU),
+            make_connected_layer(32, 10, SOFTMAX)]
+    return make_net(l)
+
 def conv_net():
+    # How many operations are needed for a forard pass through this network?
+    # Your answer: 
     l = [   make_convolutional_layer(32, 32, 3, 8, 3, 1, LRELU),
             make_maxpool_layer(32, 32, 8, 3, 2),
             make_convolutional_layer(16, 16, 8, 16, 3, 1, LRELU),
@@ -12,9 +23,19 @@ def conv_net():
             make_connected_layer(256, 10, SOFTMAX)]
     return make_net(l)
 
+def your_net():
+    # Define your network architecture here. It should have 5 layers. How many operations does it need for a forward pass?
+    # It doesn't have to be exactly the same as conv_net but it should be close.
+    l = [   make_connected_layer(3072, 72, RELU),
+            make_connected_layer(72, 3072, RELU),
+            make_connected_layer(3072, 72, RELU),
+            make_connected_layer(72, 3072, RELU),
+            make_connected_layer(3072, 10, SOFTMAX)]
+    return make_net(l)
+
 print("loading data...")
 train = load_image_classification_data("cifar/cifar.train", "cifar/cifar.labels")
-test  = load_image_classification_data("cifar/cifar.test",  "cifar/cifar.labels")
+test  = load_image_classification_data("cifar/cifar.test", "cifar/cifar.labels")
 print("done")
 print
 
@@ -25,7 +46,7 @@ rate = .01
 momentum = .9
 decay = .005
 
-m = conv_net()
+m = your_net()
 print("training...")
 train_image_classifier(m, train, batch, iters, rate, momentum, decay)
 print("done")
